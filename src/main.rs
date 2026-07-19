8b96763b51769b82bcab72c387cb66c7ec6c36a0
ohmycloud <ohmycloudy@gmail.com>
2026-07-20 00:49:18 +0800

    TypedHeader

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use axum::Router;
 use axum::http::header::{CONTENT_TYPE, HeaderMap, USER_AGENT};
 use axum::routing::get;
+use axum_extra::{
+    extract::TypedHeader,
+    headers::{ContentType, UserAgent},
+};
 
 async fn hello(headers: HeaderMap) -> String {
     let user_agent = headers
@@ -16,9 +20,18 @@ async fn hello(headers: HeaderMap) -> String {
     format!("User-Agent: {}, Content-Type: {}", user_agent, content_type)
 }
 
+async fn greet(
+    TypedHeader(user_agent): TypedHeader<UserAgent>,
+    TypedHeader(content_type): TypedHeader<ContentType>,
+) -> String {
+    format!("User-Agent: {}, Content-Type: {}", user_agent, content_type)
+}
+
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(hello));
+    let app = Router::new()
+        .route("/", get(hello))
+        .route("/greet", get(greet));
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
         .await
